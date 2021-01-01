864efbc727081d9904d7a6e01df97238025b80a3
PhuongTung <60902006+PhuongTung@users.noreply.github.com>
2021-01-01 16:51:44 +0700

    first-commit

--- update-messages.js
+++ update-messages.js
@@ -1,3 +1,12 @@
+/**
+ * script to use in package.json
+ * args: node update-messages.js real update messages
+*/
 
+const { exec } = require('child_process');
+let args = process.argv;
+args.splice(0, 2);
+let str = args.join(" ");
 
-console.log("Something here!");
+console.log(str);
+console.log("Updated!");
